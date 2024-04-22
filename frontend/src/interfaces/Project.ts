ad3d87bbec630f5db356db8adbcae3ea7162781e
Marc Beyer <marc.beyer1@gmx.de>
2024-04-22 09:41:23 +0100

    Query project from backend

--- frontend/src/interfaces/Project.ts
+++ frontend/src/interfaces/Project.ts
@@ -1,10 +1,21 @@
 import { MilestoneObj } from "./Milestone";
 
 export interface ProjectObj {
-    id: string;
+    id: number;
     title: string;
     estimatedEnd: string;
     startDate: string;
     description: string;
     milestones: MilestoneObj[];
+}
+
+export interface ProjectRespondsObj {
+    createdDate: string;
+    creatorId: number;
+    description: string;
+    estimateDate: string;
+    milestones: [];
+    projectId: number;
+    projectName: string;
+    startDate: string;
 }
